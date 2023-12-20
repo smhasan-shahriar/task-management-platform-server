f89e852968287c8787e71de831259f2b08edf6d3
Ador Shahriar <ador.shahriar@gmail.com>
2023-12-21 00:35:04 +0600

    mongoDB database added

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+//admin
+//SR04ivkKKZ4FI1p5
 
-app.get('/', (req, res) => {
-    res.send('server is running')
-})
+const uri =
+  "mongodb+srv://admin:SR04ivkKKZ4FI1p5@cluster0.cmi4wzm.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(port, ()=> {
-    console.log(`server is running on port ${port}`)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnect = async () => {
+  try {
+    client.connect();
+    console.log("DB Connected Successfully✅");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
