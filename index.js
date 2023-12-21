3475a882ccb5dff86d7cb17a87f76ff2d9adefd0
Ador Shahriar <ador.shahriar@gmail.com>
2023-12-21 16:53:14 +0600

    delete functionality created

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-//admin
-//SR04ivkKKZ4FI1p5
 
 const uri =
-  "mongodb+srv://admin:SR04ivkKKZ4FI1p5@cluster0.cmi4wzm.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cmi4wzm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
